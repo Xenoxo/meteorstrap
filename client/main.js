1b507171195e2a71fda2954a2561c09ded6b47a8
Xenoxo <timwang@Tims-MacBook-Air.local>
2015-10-04 22:19:01 -0700

    Configure flapper javascript library add on
    Worked and examined the flapper library.
    
    Wanted to make the flapper numbers take place as soon as user hits the
    main page, therefore moved flapper boxes from template.main.event to
    template.main.onRendered.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
+Template.main.onRendered(function() {
+		$('#display').flapper().val(123456).change();
+});
+
+/*
 Template.main.events({
 	//'click #clickme':function(){alert("BUTTS!");}
 	'click #clickme':function(){
-		$('#display').flapper().val(12345).change();
+		$('#display').flapper({
+		}).val(123).change();
 	}
 	//'click #clickme': function(){$('#display').flapper(options).val(12345).change()};
 	
-});
+}); */
