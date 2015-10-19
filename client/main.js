6cfae97fd55970ade53a6f8e43585e252bac3e8d
Xenoxo <timwang@Tims-MacBook-Air.local>
2015-10-18 18:08:02 -0700

    Set up parser framework
    Next step is to add functionality so that parser fully parses the data
    and stores into mongo

--- client/main.js
+++ client/main.js
@@ -6,21 +6,15 @@ Template.main.onRendered(function() { //calculations need to run in here as iaff
 	timing: 250,
 	min_timing: 20,
 	}).val(displayTotal).change(); //DataTest is being derived from tweetdata.js
-console.log();
-/*
-for (var i = megadump.length - 1; i >= 0; i--) {
-    console.log(megadump[i].text);
-  }
-  var str = "#yo 988 this is a bunch of text";
-  var splitstr = jQuery.map(str.split(' '),function(n){
-    return n;
-  });
-  console.log(splitstr[0]);
-*/
+
+    reatedAt = new Date().valueOf();
+    console.log(Math.floor(reatedAt/100));
+
+  
 }); 
 
 Template.main.helpers({
   getTwitter: function () {
-    return ReactiveMethod.call("twitter");
+    return ReactiveMethod.call("twitterCall");
   }
 });
--- server/methods.js
+++ server/methods.js
@@ -1,18 +1,24 @@
 Meteor.methods({
-	twitter: function(){
-		this.unblock();
-		try {
-			console.log("SUCCESS111");
-			var url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=2&oauth_consumer_key=QOkwkQ5xUWBSLiKI69JxHw2jt&oauth_token=600434038-AjJuNPRiSOkuK1FwXqQdR9egfu0304fhvQe3ueR9&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1444882234&oauth_nonce=pTcIvE&oauth_version=1.0&oauth_signature=03%2BFaJhpL1gSyrJCm0sIl4SS3T8%3D";
-			console.log(url);
-			var result = Meteor.http.get(url);
-			var datablob = result.data;
-			console.log(result);
-			console.log("SUCCESS!");
-			return result.data.response;
-		} catch (error){
-			console.log(error);
-			return error;
-		}
+	twitterCall: function(){
+		Twit.get('statuses/user_timeline', {user_id:600434038, count:5, exclude_replies:true }, function(err, data, response) {
+			var str;
+			for (var i = data.length - 1; i >= 0; i--) {
+				str = data[i].text.split(" ");
+				tagPos = str.indexOf("#paid");
+				if(tagPos >= 0){
+					console.log(str[tagPos] + str[tagPos+1]);
+					//change this to add data to the mongoDB
+				}
+				
+				
+			}
+		});
 	}
+});
+
+Twit = new TwitMaker({
+    consumer_key:'QOkwkQ5xUWBSLiKI69JxHw2jt',
+    consumer_secret:'1uSahkvGheSNGQR3K9rf9ao1SYO289Ph0DLEuTvwor3MqmllL4',
+    access_token:'600434038-AjJuNPRiSOkuK1FwXqQdR9egfu0304fhvQe3ueR9',
+    access_token_secret:'fIHXdJlLgqx6SAd0C8PoKF1ubteagbYtoG494A6nOBqQs'
 });
