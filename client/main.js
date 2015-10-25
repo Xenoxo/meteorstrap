626fff85e5844a087d4176560914b4296f17c359
Xenoxo <timwang@Tims-MacBook-Air.local>
2015-10-24 22:06:37 -0700

    Debug backend to correct api call + added front end hook
    Created filter so only newest of tweets are being added. Also got
    totals working correctly!

--- client/main.js
+++ client/main.js
@@ -1,16 +1,11 @@
 Template.main.onRendered(function() { //calculations need to run in here as iaffects the main page
-	var currentTotal = 0;
+  var currentTotal = TweetData.findOne({},{sort:{tweetID:-1}}).total;
   var displayTotal = currentTotal.toFixed(2);
   $('#display').flapper({
-	width:8,
-	timing: 250,
-	min_timing: 20,
-	}).val(displayTotal).change(); //DataTest is being derived from tweetdata.js
-
-    reatedAt = new Date().valueOf();
-    console.log(Math.floor(reatedAt/100));
-
-  
+  width:8,
+  timing: 250,
+  min_timing: 20,
+  }).val(displayTotal).change(); //DataTest is being derived from tweetdata.js
 }); 
 
 Template.main.helpers({
--- server/methods.js
+++ server/methods.js
@@ -8,15 +8,20 @@ var Twit = new TwitMaker({
 });
 
 function twitterCall(){
-		var sinceLast = TweetData.findOne({},{sort:{tweetID:-1}}); 
+		var sinceLast, total;
+		sinceLast = TweetData.findOne({},{sort:{tweetID:-1}}); //returns either a blob or undefined
 		if (sinceLast) {
 			sinceLast = sinceLast.tweetID;
-		} else {
+		} else { //this case only happens if I reset the entire project's DB
 			sinceLast = 654503759737831400; //hardcoded, tried 0, didn't work, didn't look into why
+			total = 0;
 		}
-		console.log("The sinceLast ID "+ sinceLast);
+		
+		console.log("Start of call and the sinceLast ID "+ sinceLast);
+
+
 		Twit.get('statuses/user_timeline', 
-		{user_id:600434038, since_id:sinceLast, exclude_replies:true }, 
+		{user_id:600434038, since_id:sinceLast, exclude_replies:true}, 
 		Meteor.bindEnvironment(
 			function(err, data, response) { 
 				var str,tagPos,tweetID,latestID;
@@ -26,25 +31,32 @@ function twitterCall(){
 					str = data[i].text.split(" ");
 					tagPos = str.indexOf("#paid");
 					tweetID = data[i].id;
-					//latestID = TweetData.findOne({},{fields:{'tweetID': 1, _id:0}},{sort:{tweetID:-1}}).tweetID;
-
-					//#paid needs exist | the inverse of (is this tweetID already in the DB?)
-					if(tagPos >= 0 && !TweetData.findOne({},{sort:{tweetID:-1}})){ //tagPos returns -1 if there is no #paid value 
+					//#paid needs exist | if the current tweet is newer than the latest in the DB 
+					if(tagPos >= 0 && tweetID > sinceLast ){ //tagPos returns -1 if there is no #paid value 
 						var amount = Number(str[tagPos+1]);
-						//var total = TweetData.find({},{sort:{_id:-1},limit:1}).fetch() + amount;
+						if(total === 0){
+							total = amount;
+						}else{
+							total = TweetData.findOne({},{sort:{tweetID:-1}}).total + amount;
+							//console.log("the total is "+ total);
+						}
 						latestID = tweetID;
 						TweetData.insert({
 							amount: amount,
-							//total: total,
+							total: total,
 							tweetID: data[i].id,
 							tweetBlob: data
 						});
-						console.log("This shouldn't show up " +tweetID);
+						console.log("Total in the loop right now "+total);			
 					}
 				}
+			console.log("App total is " + TweetData.findOne({},{sort:{tweetID:-1}}).total);
 			}
+
 		)
 		);
 		
 	}
 Meteor.setInterval(twitterCall, 5000);
+
+//latestID = TweetData.findOne({},{fields:{'tweetID': 1, _id:0}},{sort:{tweetID:-1}}).tweetID;
