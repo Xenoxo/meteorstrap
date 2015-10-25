0c379540d9579da1264aa62ddcce43788aa7c4ee
Xenoxo <timwang@Tims-MacBook-Air.local>
2015-10-24 20:08:34 -0700

    Add since_id into the API query and change the conditionals to work with that

--- server/methods.js
+++ server/methods.js
@@ -8,16 +8,28 @@ var Twit = new TwitMaker({
 });
 
 function twitterCall(){
+		var sinceLast = TweetData.findOne({},{sort:{tweetID:-1}}); 
+		if (sinceLast) {
+			sinceLast = sinceLast.tweetID;
+		} else {
+			sinceLast = 654503759737831400; //hardcoded, tried 0, didn't work, didn't look into why
+		}
+		console.log("The sinceLast ID "+ sinceLast);
 		Twit.get('statuses/user_timeline', 
-		{user_id:600434038, count:10, exclude_replies:true }, 
+		{user_id:600434038, since_id:sinceLast, exclude_replies:true }, 
 		Meteor.bindEnvironment(
 			function(err, data, response) { 
-				var str,tagPos,tweetID;
+				var str,tagPos,tweetID,latestID;
+
+				//parses through each tweet
 				for (var i = data.length - 1; i >= 0; i--) {
 					str = data[i].text.split(" ");
 					tagPos = str.indexOf("#paid");
 					tweetID = data[i].id;
-					if(tagPos >= 0 && tweetID > latestID){ //return -1 if there is no #paid value
+					//latestID = TweetData.findOne({},{fields:{'tweetID': 1, _id:0}},{sort:{tweetID:-1}}).tweetID;
+
+					//#paid needs exist | the inverse of (is this tweetID already in the DB?)
+					if(tagPos >= 0 && !TweetData.findOne({},{sort:{tweetID:-1}})){ //tagPos returns -1 if there is no #paid value 
 						var amount = Number(str[tagPos+1]);
 						//var total = TweetData.find({},{sort:{_id:-1},limit:1}).fetch() + amount;
 						latestID = tweetID;
@@ -27,12 +39,12 @@ function twitterCall(){
 							tweetID: data[i].id,
 							tweetBlob: data
 						});
-						//console.log(latestID);
+						console.log("This shouldn't show up " +tweetID);
 					}
 				}
 			}
 		)
 		);
-		console.log(TweetData.findOne({},{fields:{'amount': 1, _id:0}},{sort:{tweetID:-1}}));
+		
 	}
 Meteor.setInterval(twitterCall, 5000);
